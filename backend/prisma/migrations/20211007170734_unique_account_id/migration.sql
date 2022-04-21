38116605acd5924062a4bf9f53a6ebed3bdebd7f
Nickolaj Jepsen <nij@billigvvs.dk>
2022-04-21 17:59:07 +0200

    Update schema

--- backend/prisma/migrations/20211007170734_unique_account_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[accountId]` on the table `Player` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Player_accountId_key" ON "Player"("accountId");
